9906829a6bb8ba4142b0f2863152c30ee06b92c3
Farudll <FraudLl@163.com>
2020-03-08 06:36:01 +0800

    匹配路由，浏览器展现成果，我是主页..

---
+++ meiduo_mall/apps/index/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+# from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+    def get(self, request):
+        """
+        主页
+        :param request:
+        :return:
+        """
+        return HttpResponse("我是 主页 ....")
+        # return render(request, 'index.html')
